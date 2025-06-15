96a019b0b126a5d78f7ee0041a6bc5f477b9cc08
okafor ebuka <122581334+grunde1234@users.noreply.github.com>
2025-06-15 23:54:43 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '/api':{
         target: 'https://django-article-api.onrender.com/api',
         changeOrigin: true,
-        rewrite: (path)=>path.replace(/^\/api/, ' ')
+        rewrite: (path)=>path.replace(/^\/api/, '')
       }
     }
   }
